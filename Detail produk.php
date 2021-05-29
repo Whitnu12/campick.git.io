de04a83c305810a70175964c5187704e4a454321
Infineett <82891856+Infineett@users.noreply.github.com>
2021-05-29 11:19:37 +0700

    Add files via upload
    wok

---
+++ Detail produk.php
@@ -0,0 +1,158 @@
+<html>
+
+<head>
+    <link rel=" stylesheet" href="CSS/detail_produk.css" media="screen">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Product</title>
+</head>
+
+
+<div class="navbar">
+    <ul>
+        <li> <a href="index.php"> Login </a> </li>
+        <li> <a href="#">About us</a></li>
+        <li> <a href="#">Galeri</a></li>
+        <li> <a href="#" class="active">Product</a> </li>
+        <li> <a href="#">Home</a></li>
+        <li style="float: left; padding-left:20px ; margin: 10px;"> <img src="./IMG/Logo Campick .png" id="logo">
+        </li>
+    </ul>
+</div>
+
+<div id="gallerywrapper">
+    <div id="gallery">
+        <div id="pic1">
+            <img src="img/produk/alas.jpg" height="350" width="500" alt="Image 1">
+            <a class="previous" href="#pic5">&lt;</a>
+            <a class="next" href="#pic2">&gt;</a>
+            <h3>ALAS</h3>
+            <p>Text of image 1.</p>
+        </div>
+        <div id="pic2">
+            <img src="img/produk/alas.jpg" height="350" width="500" alt="Image 2">
+            <a class="previous" href="#pic1">&lt;</a>
+            <a class="next" href="#pic3">&gt;</a>
+            <h3>ALAS</h3>
+            <p>Text of image 2.</p>
+        </div>
+        <div id="pic3">
+            <img src="img/produk/alas.jpg" height="350" width="500" alt="Image 3">
+            <a class="previous" href="#pic2">&lt;</a>
+            <a class="next" href="#pic4">&gt;</a>
+            <h3>ALAS</h3>
+            <p>Text of image 3.</p>
+        </div>
+        <div id="pic4">
+            <img src="img/produk/alas.jpg" height="350" width="500" alt="Image 4">
+            <a class="previous" href="#pic3">&lt;</a>
+            <a class="next" href="#pic5">&gt;</a>
+            <h3>ALAS</h3>
+            <p>Text of image 4.</p>
+        </div>
+        <div id="pic5">
+            <img src="img/produk/alas.jpg" height="350" width="500" alt="Image 5">
+            <a class="previous" href="#pic4">&lt;</a>
+            <a class="next" href="#pic1">&gt;</a>
+            <h3>ALAS</h3>
+            <p>Text of image 5.</p>
+        </div>
+    </div>
+</div>
+<div id="menu">
+    <div id="info">
+        <h3>Alas Tikar</h3>
+        <p>Rp.300.000,00</p>
+    </div>
+    <div id="warna">
+        <p>Warna : </p>
+        <input class="abu-abu" type="submit" value="abu-abu">
+        <input class="hitam" type="submit" value="hitam">
+        <input class="coklat" type="submit" value="coklat">
+    </div>
+    <div id="jumlah">
+        <p>Jumlah: </p>
+
+
+
+    </div>
+    <div id="button">
+        <input class="keranjang" type="submit" value="Masukkan Favorit">
+        <input class="booking" type="submit" value="Booking">
+    </div>
+</div>
+<div id="spesifikasi">
+    <h3>Spesifikasi Produk</h3>
+    <table>
+        <tr>
+            <td>Ukuran</td>
+            <td>&emsp;&emsp;2m X 1m</td>
+        </tr>
+        <tr>
+            <td>Berat</td>
+            <td>&emsp;&emsp;300 gr</td>
+        </tr>
+        <tr>
+            <td>Kapasitas</td>
+            <td>&emsp;&emsp;1 Orang</td>
+        </tr>
+    </table>
+</div>
+
+<div id="deskripsi">
+    <h3>Deskripsi Prodduk</h3>
+    <p>Matras camping berbahan waterproof ini bisa dengan mudah Anda bawa ketika ada kegiatan travel di alam bebas,
+        seperti piknik, camping, maupun hiking. Memiliki ukuran yang luas, tikar kemping juga bisa dengan mudah Anda
+        lipat dan dimasukkan ke dalam tas.</p>
+</div>
+
+<div id="penilaian">
+    <h3>Penilaian Produk</h3>
+    <div id="ulasan">
+        <img style="float: left;" src="img/profile-user.png">
+        <p>@wtf</p>
+        <div class='rating'>
+            <div id="r1" class="rate_widget">
+                <div class="star_1 ratings_stars"></div>
+                <div class="star_2 ratings_stars"></div>
+                <div class="star_3 ratings_stars"></div>
+                <div class="star_4 ratings_stars"></div>
+                <div class="star_5 ratings_stars"></div>
+            </div>
+        </div>
+        <p>gak nyesel nyewa di sini</p>
+    </div>
+    <div id="ulasan">
+        <img style="float: left;" src="img/profile-user.png">
+        <p>@wtf</p>
+        <div class='rating'>
+            <div id="r1" class="rate_widget">
+                <div class="star_1 ratings_stars"></div>
+                <div class="star_2 ratings_stars"></div>
+                <div class="star_3 ratings_stars"></div>
+                <div class="star_4 ratings_stars"></div>
+                <div class="star_5 ratings_stars"></div>
+            </div>
+        </div>
+        <p>gak nyesel nyewa di sini</p>
+    </div>
+    <div id="ulasan">
+        <img style="float: left;" src="img/profile-user.png">
+        <p>@wtf</p>
+        <div class='rating'>
+            <div id="r1" class="rate_widget">
+                <div class="star_1 ratings_stars"></div>
+                <div class="star_2 ratings_stars"></div>
+                <div class="star_3 ratings_stars"></div>
+                <div class="star_4 ratings_stars"></div>
+                <div class="star_5 ratings_stars"></div>
+            </div>
+        </div>
+        <p>gak nyesel nyewa di sini</p>
+    </div>
+</div>
+
+</body>
+
+</html>
