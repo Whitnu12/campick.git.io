b90202686d3d681a33c379d54221ec8b21160472
Raehanwidi2002 <rehanwidisugiarto2002@gmail.com>
2021-06-03 13:49:20 +0700

    login admin

---
+++ Admin/index.php
@@ -0,0 +1,60 @@
+
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> 	<html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- General Metas -->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">	<!-- Force Latest IE rendering engine -->
+	<title>Login Form</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="./assets/css/base.css">
+	<link rel="stylesheet" href="./assets/css/skeleton.css">
+	<link rel="stylesheet" href="./assets/css/layout.css">
+	
+</head>
+<body>
+
+
+	<!-- Primary Page Layout -->
+
+	<div class="container">
+		
+		<div class="form-bg">
+			<form>
+				<h2>Login</h2>
+				<p><input type="text" placeholder="Username"></p>
+				<p><input type="password" placeholder="Password"></p>
+				<label for="remember">
+				  <input type="checkbox" id="remember" value="remember" />
+				  <span>Remember me on this computer</span>
+				</label>
+				<button type="submit" name="login"></button>
+			<form>
+		</div>
+
+	
+		<p class="forgot">Forgot your password? <a href="">Click here to reset it.</a></p>
+
+
+	</div><!-- container -->
+
+	<!-- JS  -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+	<script>window.jQuery || document.write("<script src='js/jquery-1.5.1.min.js'>\x3C/script>")</script>
+	<script src="./assets/js/app.js"></script>
+	
+<!-- End Document -->
+</body>
+</html>
